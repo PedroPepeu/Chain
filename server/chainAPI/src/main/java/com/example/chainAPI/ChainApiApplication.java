3e14cdc19a7dfca8689376e7948493e8afb8a46e
PedroPepeu <pedrosteamgtav@gmail.com>
2024-05-17 15:35:04 -0300

    added the spring boot, v22, spring 3.2.5, gradle-kotling, java

---
+++ server/chainAPI/src/main/java/com/example/chainAPI/ChainApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chainAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainApiApplication.class, args);
+	}
+
+}
