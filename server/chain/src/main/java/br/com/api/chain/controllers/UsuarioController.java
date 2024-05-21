0346523385098f8a127ff6a3e2de44e2ebe11a01
flavio <flavio.00000844530@unicap.br>
2024-05-21 15:00:53 -0300

    controllers ajeitados

--- server/chain/src/main/java/br/com/api/chain/controllers/UsuarioController.java
+++ server/chain/src/main/java/br/com/api/chain/controllers/UsuarioController.java
@@ -2,7 +2,12 @@ package br.com.api.chain.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.chain.repositories.UsuarioRepository;
@@ -18,6 +23,31 @@ public class UsuarioController {
 
     @GetMapping("/usuario")
     public List<Usuario> getUsuario(){
+        return Usuario.findAll();
+    }
+
+    @PostMapping("/usuario")
+    public List<Usuario> createProject(@RequestBody Usuario newUsuario) {
+        Usuario.save(newUsuario);
+        return Usuario.findAll();
+    }
+
+    @PutMapping("/usuario/{id}")
+    public List<Usuario> updateUsuario(@RequestBody Usuario updateUsuario, @PathVariable String id) {
+        Usuario.findById(id)
+            .map(usuario -> {
+                usuario.setNome(updateUsuario.getNome());
+                usuario.setSenha(updateUsuario.getSenha());
+                usuario.setEmail(updateUsuario.getEmail());
+                return Usuario.save(usuario);
+            });
+
+        return Usuario.findAll();
+    }
+
+    @DeleteMapping("/usuario/{id}")
+    public List<Usuario> deleteProject(@PathVariable String id) {
+        Usuario.deleteById(id);
         return Usuario.findAll();
     }
 }
