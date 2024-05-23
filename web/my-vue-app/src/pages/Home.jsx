5de93231df2798e0b8bac6e5c95cd33e40037f29
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-05-23 12:56:43 -0300

    achando conflitos

--- web/my-vue-app/src/pages/Home.jsx
+++ web/my-vue-app/src/pages/Home.jsx
@@ -18,8 +18,8 @@ function Home() {
               <Header></Header>
             </div>
             <div className='middle-wrapper'>
-              <div class="main-container">
-                <div class="main-content">
+              <div className="main-container">
+                <div className="main-content">
                   <h1>Eu ainda vou mudar essa página v0.1</h1>
                   <p>Desenvolver um software é uma tarefa complexa que muitas vezes envolve o uso de vários aplicativos diferentes, porém às vezes é complicado gerenciar todos os aplicativos e engenheiros de software de um projeto. Para resolver esse problema, foi idealizada a criação de um site que interligasse todos os outros aplicativos e engenheiros de software, para ajudar a melhorar a organização de um projeto de software.
                     Nesse site seria possível a criação de projetos em que pudessem ser adicionados os links dos outros aplicativos, unificando mais o projeto. Além disso, dentro dos projetos seria possível adicionar as diversas atividades necessárias e administrar o que cada engenheiro de software deve fazer. Essas atividades ajudariam a mapear o andamento do projeto e cada engenheiro poderia ver quais são as tarefas que faltam ser realizadas a partir de um calendário.
