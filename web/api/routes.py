2d3ddca68975ee2a247e0e361b905b13d4feb52f
JoaoGabrilTN <joaogt008@gmail.com>
2024-05-23 14:33:16 -0300

    pivotando o projeto

---
+++ web/api/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, flash, redirect, url_for
+from api.index import app 
+from api.forms import LoginForm
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('errors/404.html')
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    user = {'username': 'João'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
