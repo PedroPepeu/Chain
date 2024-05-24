8e9974db7f1ff963862f1d7ffe247c9597474bd7
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-05-24 03:05:28 -0300

    mensagens de exibição da tela de login

---
+++ web/api/static/js/login.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function exibirNotificacao(mensagem) {
+        Toastify({
+            text: mensagem,
+            duration: 4000
+        }).showToast();
+    }
+
+    document.getElementById("loginConteudo").addEventListener("submit", function(event) {
+        event.preventDefault();
+        var loginValido = true; // Altere para false se o login for inválido
+        var senhaValida = true; // Altere para false se a senha for inválida
+        
+        if (loginValido && senhaValida) {
+            exibirNotificacao("Login efetuado com sucesso");
+            exibirNotificacao("pepeu ajusta banco para verificar se senha e login são validos"); 
+            setTimeout(function() {
+                window.location.href = "{{ url_for('profile') }}";
+            }, 3000);
+        // Simula a verificação do banco de dados    
+        } else {
+            if (!loginValido) {
+                exibirNotificacao("Login inválido");
+            } else {
+                exibirNotificacao("Senha inválida");
+            }
+        }
+    });
+
+    document.getElementById("RecuperarConta").addEventListener("click", function(event) {
+        event.preventDefault();
+        exibirNotificacao("redirecionando para area de recuperar conta");
+        setTimeout(function() {
+            window.location.href = "{{ url_for('cadastro') }}";
+        }, 3000);
+    });
+
+    document.getElementById("cadastrarConta").addEventListener("click", function(event) {
+        event.preventDefault();
+        exibirNotificacao("redirecionando para tela de cadastro");
+        setTimeout(function() {
+            window.location.href = "{{ url_for('cadastro') }}";
+        }, 3000);
+    });
+});
