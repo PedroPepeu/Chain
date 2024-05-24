82164440456c6f6455aec826b55cba0b5a62912a
JoaoGabrilTN <joaogt008@gmail.com>
2024-05-24 02:13:28 -0300

    criação de projetos em andamento

--- web/api/static/js/profile.js
+++ web/api/static/js/profile.js
@@ -1,27 +1,11 @@
-class project{
-    constructor(){
-        this.render();
-    }
-    
-    render(){
-        this.button = document.createElement('button');
-        this.button.innerText = '+';
-        this.button.classList.add("content-add");
-
-        this.button.addEventListener('click', () => {
-            
-        });
-    }
+function addProject() {
+    var node = document.createElement('div')
+    node.style.width = '60vw';
+    node.style.height = '20rem';
+    node.style.backgroundColor = '#000';
+    node.style.marginTop = '10px';
+    node.style.marginBottom = '10px';
+    node.style.borderRadius = '10px'
+    document.getElementById("projects").appendChild(node);
 }
-
-let addProjectButton = document.getElementById("addProjectButton");
-
-addProjectButton.addEventListener('click', () => {
-    if (addProjectInput.value.trim() != "") {
-        new project(root);
-        addProjectInput.value = "";
-    }
-});
-
-let project = project();
 
