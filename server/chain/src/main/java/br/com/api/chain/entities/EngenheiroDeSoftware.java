e5a4e413517930e9a3c88a6880ca99a146cd3c6d
HeloisaJ <heloisa.00000845226@unicap.br>
2024-05-28 22:49:27 -0300

    Conexão entre anotação e engenheiro feita

--- server/chain/src/main/java/br/com/api/chain/entities/EngenheiroDeSoftware.java
+++ server/chain/src/main/java/br/com/api/chain/entities/EngenheiroDeSoftware.java
@@ -1,10 +1,15 @@
 package br.com.api.chain.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +27,10 @@ public class EngenheiroDeSoftware {
 
     @Column(name="email")
     private String email;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "engenheiro_de_software_id")
+    private List<Anotacao> anotacoes;
 
     public EngenheiroDeSoftware(){}
 
