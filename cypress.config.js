0877387cc94f0c12e6a900654864c3d851e2221f
PedroPepeu <pedrosteamgtav@gmail.com>
2024-06-06 09:38:39 -0300

    ci

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    // Configure your E2E tests here
-    specPattern: "testChain/cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    supportFile: "testChain/cypress/support/e2e.ts",
-  },
-})
