9992457b7c3480066d4dfce0ba8aaf02618e58d2
PedroPepeu <pedrosteamgtav@gmail.com>
2024-06-06 13:52:00 -0300

    update wrong test

--- testChain/cypress/e2e/post-login-screen/project-user-screen.cy.ts
+++ testChain/cypress/e2e/post-login-screen/project-user-screen.cy.ts
@@ -1,9 +1,1 @@
 let linkProjectUser = "http://localhost:8080/project";
-
-describe('Check-project-page', function() {
-    it('Check-home-component', function() {
-        cy.visit(linkProjectUser);
-
-        cy.get('#').should('exist');
-    });
-});
